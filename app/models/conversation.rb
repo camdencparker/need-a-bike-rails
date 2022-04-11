f71f2dfd59ed776849808fc6eb370fcd6d747081
Camden <camdencparker@gmail.com>
2022-04-11 10:37:00 -0700

    adds associations and validations

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,2 +1,9 @@
 class Conversation < ApplicationRecord
+  has_many :messages, dependent: :destroy 
+  belongs_to :leaser, class_name: "User" 
+  belongs_to :renter, class_name: "User"
+  
+  
+  #prevents multiple conversations between two users
+  validates :renter_id, uniqueness: {scope: :leaser_id}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  has_many :listings, dependent: :destroy
+  # has_many :renter_converstations, class_name: "Conversation", foreign_key: "leaser_id"
+  # has_many :leaser_converstations, class_name: "Conversation", foreign_key: "renter_id"
+  # has_many :leasers, through: :leaser_converstations, source: :renter
+  # has_many :renters, through: :renter_converstations, source: :leaser
+  # DONT ACTUALLY NEED THIS DATA YET
+  has_many :messages, dependent: :destroy
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
+  
+  def conversations
+    Conversation.where("renter_id = ? OR leaser_id = ?", id, id)
+  end
 end
