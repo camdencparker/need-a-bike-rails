d24765a1a4f9e8346be93f9a3529058c6917e8a0
Camden <camdencparker@gmail.com>
2022-04-20 09:47:18 -0700

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,29 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+   #User routes
+   post "/users" => "users#create"
+   get "/users/:id" => "users#show"
+   delete "/users/:id" => "users#destroy"
+ 
+   #Listing routes
+   get "/listings" => "listings#index"
+   post "/listings" => "listings#create"
+   get "/listings/:id" => "listings#show"
+   patch "/listings/:id" => "listings#update"
+   delete "/listings/:id" => "listings#destroy"
+ 
+   #Conversation routes
+   get "/conversations" => "conversations#index"
+   post "/conversations" => "conversations#create"
+   get "/conversations/:id" => "conversations#show"
+   delete "/conversations/:id" => "conversations#destroy"
+   # destroy method not in current use for MVP
+ 
+   #Message routes
+   post "/messages" => "messages#create"
+ 
+   #Authentication
+   post "/sessions" => "sessions#create"
+ 
 end
